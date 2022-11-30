97f3c6e7cb178fe4da9ff7a016fad26a2c5b96a3
imane zaki <imane_zaki@MacBook-Air-de-imane.local>
2022-11-30 15:43:51 +0100

    map + et graph 1

--- script2.js
+++ script2.js
@@ -1,4 +1,4 @@
-let map = L.map('map').setView([47.218, -1.553], 13);
+const map = L.map('map').setView([47.218, -1.553], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -19,3 +19,36 @@ fetch ("https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=24440040
             L.marker([long[i], lat[i]]).addTo(map);
         }
     })
+
+//intégration du comptage de vélo dans un graphique
+
+const ctx = document.getElementById('myChart');
+
+fetch("https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_comptages-velo-nantes-metropole&q=&sort=jour&facet=boucle_num&facet=libelle&facet=jour&facet=probabilite_presence_anomalie&facet=jour_de_la_semaine&facet=boucle_libelle&facet=vacances_zone_b")
+    .then((resp) => resp.json())
+    .then(data => {
+        const years=[]
+        const count=[]       
+        for (i=0 ; i<data.facet_groups[2].facets.length ; i++) {
+            years.push(data['facet_groups'][2]['facets'][i]['name'])
+            count.push(data['facet_groups'][2]['facets'][i]['count'])
+        }
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: years,
+                datasets: [{
+                    label: 'Nombre de passages de vélos',
+                    data: count,
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales:{
+                    y: {
+                        beginAtZero: true 
+                    }
+                }
+            }
+        })
+    });
